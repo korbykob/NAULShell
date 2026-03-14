aec429453838ab8efd4866acd953229a956ba7e0
korbykob <korbykobs@gmail.com>
2026-03-14 05:08:51 +0000

    Add removed compile args

--- build.sh
+++ build.sh
@@ -3,12 +3,14 @@ set -e
 
 mkdir -p bin/commands
 
-gcc $PROGRAM_COMPILER_FLAGS -Iinclude src/commands/help.c -o bin/commands/help.o
-gcc $PROGRAM_COMPILER_FLAGS -Iinclude src/commands/exit.c -o bin/commands/exit.o
-gcc $PROGRAM_COMPILER_FLAGS -Iinclude src/commands/ping.c -o bin/commands/ping.o
-gcc $PROGRAM_COMPILER_FLAGS -Iinclude src/commands/clear.c -o bin/commands/clear.o
-gcc $PROGRAM_COMPILER_FLAGS -Iinclude src/commands/kys.c -o bin/commands/kys.o
-gcc $PROGRAM_COMPILER_FLAGS -Iinclude src/shell.c -o bin/shell.o
+COMPILER_FLAGS="$PROGRAM_COMPILER_FLAGS -Iinclude -g -fno-omit-frame-pointer -mavx2 -O2 -fvect-cost-model=dynamic -Wall -Wextra -Werror"
+
+gcc $COMPILER_FLAGS src/commands/help.c -o bin/commands/help.o
+gcc $COMPILER_FLAGS src/commands/exit.c -o bin/commands/exit.o
+gcc $COMPILER_FLAGS src/commands/ping.c -o bin/commands/ping.o
+gcc $COMPILER_FLAGS src/commands/clear.c -o bin/commands/clear.o
+gcc $COMPILER_FLAGS src/commands/kys.c -o bin/commands/kys.o
+gcc $COMPILER_FLAGS src/shell.c -o bin/shell.o
 
 ld -r bin/commands/help.o bin/commands/exit.o bin/commands/ping.o bin/commands/clear.o bin/commands/kys.o bin/shell.o -o bin/final.o
 
